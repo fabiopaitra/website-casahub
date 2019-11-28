33db1b608eb0df480c14b3f6f264a7c490114a37
Fabio Paitra <fabio@s21digital.com>
2019-11-27 21:51:58 -0300

    Hero Section Responsive Correction

--- src/components/home/01-benefits.vue
+++ src/components/home/01-benefits.vue
@@ -1,49 +1,50 @@
 <template lang="pug">
-section.section.container
-  h2.title.has-text-centered
-    | O gerenciador de canais das plataformas imobiliárias
-  .content
-    p
-    | Imobiliárias utilizam a Casahub para gerenciar a publicação e performance dos seus imóveis. Nossa solução fornece automação completa e integrada com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
-    .columns.section
-      .column
-        .columns
-          .column
-            figure.image
-              img(src="../../assets/images/anuncie-nos-principais-portais.png", alt="")
-          .column
-            h3.title.is-4
-              | Anuncie nos principais portais
-            p
-              | Nossa solução fornece um conjunto completo e integrado com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
-        .columns
-          .column
-            figure.image
-              img(src="../../assets/images/anuncie-nos-principais-portais.png", alt="")
-          .column
-            h3.title.is-4
-              | Receba dicas de desempenho
-            p
-              | Nossa solução fornece um conjunto completo e integrado com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
-      .column
-        .columns
-          .column
-            figure.image
-              img(src="../../assets/images/anuncie-nos-principais-portais.png", alt="")
-          .column
-            h3.title.is-4
-              | Acompanhe a performance
-            p
-              | Nossa solução fornece um conjunto completo e integrado com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
-        .columns
-          .column
-            figure.image
-              img(src="../../assets/images/anuncie-nos-principais-portais.png", alt="")
-          .column
-            h3.title.is-4
-              | Direcione a divulgação
-            p
-              | Nossa solução fornece um conjunto completo e integrado com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
+section.container
+  .content.columns
+    .column.is-three-fifths.is-offset-one-fifth
+      h2.title.is-one.has-text-centered
+        | O gerenciador de canais das plataformas imobiliárias
+      p.subtitle
+        | Imobiliárias utilizam a Casahub para gerenciar a publicação e performance dos seus imóveis. Nossa solução fornece automação completa e integrada com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
+  .columns.section
+    .column
+      .columns
+        .column
+          figure.image
+            img(src="../../assets/images/anuncie-nos-principais-portais.png", alt="")
+        .column
+          h3.title.is-4
+            | Anuncie nos principais portais
+          p
+            | Nossa solução fornece um conjunto completo e integrado com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
+      .columns
+        .column
+          figure.image
+            img(src="../../assets/images/anuncie-nos-principais-portais.png", alt="")
+        .column
+          h3.title.is-4
+            | Receba dicas de desempenho
+          p
+            | Nossa solução fornece um conjunto completo e integrado com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
+    .column
+      .columns
+        .column
+          figure.image
+            img(src="../../assets/images/anuncie-nos-principais-portais.png", alt="")
+        .column
+          h3.title.is-4
+            | Acompanhe a performance
+          p
+            | Nossa solução fornece um conjunto completo e integrado com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
+      .columns
+        .column
+          figure.image
+            img(src="../../assets/images/anuncie-nos-principais-portais.png", alt="")
+        .column
+          h3.title.is-4
+            | Direcione a divulgação
+          p
+            | Nossa solução fornece um conjunto completo e integrado com os principais marketplaces de venda e aluguel de imóveis do Brasil e do mundo.
 
 </template>
 
