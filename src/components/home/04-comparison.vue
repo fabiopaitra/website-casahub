7d42f34fd3ab6620f4257421c9d816d5b956272b
Fabio Paitra <fabio@s21digital.com>
2019-12-02 17:57:48 -0300

    css background hack for safari

--- src/components/home/04-comparison.vue
+++ src/components/home/04-comparison.vue
@@ -5,60 +5,61 @@ section.section.container.content
   .columns.container
     .column
       .table-container
-        table.table.is-hoverable
-          thead.gradient-1
+        table.table.is-hoverable.gradient-1
+          thead
             tr
               th.has-text-white.padding-1 Recurso
               th.has-text-white.padding-1 Starter
               th.has-text-white.padding-1 Professional
               th.has-text-white.padding-1 Enterprise
           tbody
-            tr
+            tr.has-background-white
               td.padding-1 Preço
               td.padding-1.subtitle.has-text-weight-bold Gratuíto
               td.padding-1.subtitle.has-text-weight-bold R$ 69,90
               td.padding-1.subtitle.has-text-weight-bold R$ 179,90
-            tr  
+            tr.has-background-white  
               td.padding-1 Website
               td.padding-1 Sudomínio
               td.padding-1 Domínio Próprio
               td.padding-1 Domínio Próprio
-            tr  
-              td.padding-1 Layouts
+            tr.has-background-white  
+              td.padding-1 Layouts 
+                span.tag.is-info novo
               td.padding-1 Template padrão
               td.padding-1 Template personalizáveis
               td.padding-1 Layout exclusivos
-            tr  
+            tr.has-background-white  
               td.padding-1 Busca de imóveis
               td.padding-1 Busca no casahub.com.br
               td.padding-1 Busca no casahub.com.br
               td.padding-1 Destaque no casahub.com.br
-            tr  
+            tr.has-background-white  
               td.padding-1 Uploads
               td.padding-1 5 imóveis
               td.padding-1 30 imóveis
               td.padding-1 200 imóveis
-            tr  
+            tr.has-background-white  
               td.padding-1 Dicas de negócio
               td.padding-1 Mercado
               td.padding-1 Mercado, portais
               td.padding-1 Mercado, portais, negócios
-            tr
+            tr.has-background-white
               td.padding-1 Usuários
               td.padding-1 1 usuário
               td.padding-1 2 usuários
               td.padding-1 3 usuários
-            tr  
+            tr.has-background-white  
               td.padding-1 Integração com portais
               td.padding-1 Não incluso
               td.padding-1 Integração com portais
               td.padding-1 Integração com portais
-            tr  
+            tr.has-background-white  
               td.padding-1 Dados de desempenho
               td.padding-1 Não incluso
               td.padding-1 Não incluso
               td.padding-1 Relatórios de performance
-            tr  
+            tr.has-background-white  
               td.padding-1 Anúncio nos portais
               td.padding-1 Não incluso
               td.padding-1 Anúncios nos portais
